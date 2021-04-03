eefefd7ff4c40ee2153e1dbe2dab3c6ce980b1c8
Kevin Wang <kev.w.713@utexas.edu>
2021-04-03 15:53:24 -0500

    Extract Player class

---
+++ src/main/java/com/jitterted/ebp/blackjack/Player.java
@@ -0,0 +1,42 @@
+package com.jitterted.ebp.blackjack;
+
+public class Player {
+
+    private int playerBalance;
+    private int playerBet;
+
+    public Player() {
+        this.playerBalance = 0;
+        this.playerBet = 0;
+    }
+
+    public Player(int playerBalance, int playerBet) {
+        this.playerBalance = playerBalance;
+        this.playerBet = playerBet;
+    }
+
+    public void playerDeposits(int amount) {
+        playerBalance += amount;
+    }
+
+    public void playerBets(int betAmount) {
+        playerBet = betAmount;
+        playerBalance -= betAmount;
+    }
+
+    public int playerBalance() {
+        return playerBalance;
+    }
+
+    public void playerWins() {
+        playerBalance += playerBet * 2;
+    }
+
+    public void playerLoses() {
+        playerBalance += playerBet * 0;
+    }
+
+    public void playerTies() {
+        playerBalance += playerBet * 1;
+    }
+}
